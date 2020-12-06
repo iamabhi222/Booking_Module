29dd2eeb3f962b1dc149f79f98c7b3d2793b2ed0
Abhishek Ranjan <iamabhi222@gmail.com>
2020-12-06 16:55:28 +0530

    json

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-const port = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3004;
 
 server.use(router);
 server.listen(PORT, () => {

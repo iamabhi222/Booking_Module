1407dd2b43c09822f745332413e963d54e09487c
Abhishek Ranjan <iamabhi222@gmail.com>
2020-12-06 09:57:30 +0530

    heroku json server

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
+const port = process.env.PORT || 3004;
+
 server.use(router);
-server.listen(3004, () => {
+server.listen(PORT, () => {
   console.log("JSON Server is running");
 });

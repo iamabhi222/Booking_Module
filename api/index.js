036423a91614c8982f015cf6b69d61a69e4087f4
Abhishek Ranjan <iamabhi222@gmail.com>
2020-12-07 15:08:28 +0530

    express

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const jsonServer = require("json-server");
+const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 const PORT = process.env.PORT || 3004;
 
